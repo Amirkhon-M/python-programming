6aed42a6bc4d94c8206a0160d48ca13b2781da8b
Amirkhon <amirkhonmahmadaminov@gmail.com>
2025-08-06 11:53:29 +0500

    Turtle house

---
+++ House/solution.py
@@ -0,0 +1,21 @@
+import turtle
+
+turtnoise = turtle.Turtle()
+
+turtnoise.color('brown')
+turtnoise.rt(90)
+turtnoise.fd(200)
+turtnoise.lt(90)
+turtnoise.fd(200)
+turtnoise.lt(90)
+turtnoise.fd(200)
+turtnoise.lt(90)
+turtnoise.fd(200)
+
+turtnoise.color('black')
+turtnoise.rt(120)
+turtnoise.fd(200)
+turtnoise.rt(120)  
+turtnoise.fd(200)
+
+turtle.done()
